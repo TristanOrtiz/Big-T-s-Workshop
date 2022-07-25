136896f8ee7d4107e2d6e9e7c24c382de9c3191a
TristanOrtiz <tristanortiz75@gmail.com>
2022-07-25 11:34:20 +0900

    Revert "Revert "Add project files.""
    This reverts commit a9d0e9435be8bb0084b3a7e8276a736c7a8a9b7e.

---
+++ MyNewApplication/Data/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace MyNewApplication.Data
+{
+    public class Contact
+    {
+        public Contact(string name, string email, string message)
+        {
+            Name = name;
+            Email = email;
+            Message = message;
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
